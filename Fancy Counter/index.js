4433a731962fca1365c31fd632e78e02b053e70b
Yeboah-Godfred <ygodfred3@gmail.com>
2025-02-15 02:35:20 +0000

    Work done

--- Fancy Counter/index.js
+++ Fancy Counter/index.js
@@ -1,3 +1,7 @@
+const mainCounterEl = document.querySelector(".main-counter")
+
+const headingEl = document.querySelector(".heading")
+
 const buttonIncreaseEl = document.querySelector(".counter-button-increase");
 
 const buttonResetEl = document.querySelector(".reset-counter");
@@ -9,19 +13,34 @@ const counterValue = document.querySelector(".counter-value");
 const increaseValue = () => {
     const currentValue = counterValue.textContent;
     const currentValueAsNumber = +currentValue;
-    const newValue = currentValueAsNumber + 1;
+    let newValue = currentValueAsNumber + 1;
+    if(newValue > 5){
+        newValue = 5;
+        mainCounterEl.classList.add("main-counter-limit");
+        headingEl.textContent = "MAX LIMIT!";
+        buttonIncreaseEl.disabled = true;
+    }
     counterValue.textContent = newValue;
 }
 
 const decreaseValue = () => {
     const currentValue = counterValue.textContent;
     const currentValueAsNumber = +currentValue;
-    const newValue = currentValueAsNumber - 1;
+    let newValue = currentValueAsNumber - 1;
+    if(newValue < 0){
+        newValue = 0;
+    }else if(newValue <= 5){
+        mainCounterEl.classList.remove("main-counter-limit")
+        buttonIncreaseEl.disabled = false;
+    }
     counterValue.textContent = newValue;
 }
 
 const resetButton = () => {
     counterValue.textContent = 0;
+    mainCounterEl.classList.remove("main-counter-limit")
+    headingEl.textContent = "COUNT IT";
+    buttonIncreaseEl.disabled = false;
 }
 
 buttonIncreaseEl.addEventListener("click", increaseValue);
