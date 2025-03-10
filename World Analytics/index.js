3ef9282e99e79ce14bbb06bb14c89c17f945d14d
Yeboah-Godfred <ygodfred3@gmail.com>
2025-03-10 10:53:46 +0000

    facebook and twitter codes

--- World Analytics/index.js
+++ World Analytics/index.js
@@ -1,11 +1,21 @@
 const textareaEl = document.querySelector(".textarea");
+const charactersEl = document.querySelector(".stat-number-characters");
+const wordsEl = document.querySelector(".stat-number-words");
+const twitterEl = document.querySelector(".stat-number-twitter");
+const facebookEl = document.querySelector(".stat-number-facebook");
+
+
 
 const textFunction = () => {
     const numberOfChars = textareaEl.value.length;
-
-    const charactersEl = document.querySelector(".stat-number-characters");
-
+    const twitterCharsLeft = 280 -numberOfChars;
+    const facebookCharsLeft = 2200 -numberOfChars;
+    
     charactersEl.textContent = numberOfChars;
+    twitterEl.textContent = twitterCharsLeft;
+    facebookEl.textContent = facebookCharsLeft;
 }
+    
+
 
 textareaEl.addEventListener("input", textFunction);
