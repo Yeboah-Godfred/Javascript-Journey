208cfe081a5c76c5f7085a529624004edb6f143f
Yeboah-Godfred <ygodfred3@gmail.com>
2025-03-11 17:31:20 +0000

    first lesson

---
+++ Intermediate Javascript/index.js
@@ -0,0 +1,11 @@
+const words = "Wo baa ha b3 y33 d3n";
+
+console.log(words);
+
+const price = 369.56;
+
+console.log(price);
+
+const ages = [12, 56, 39, 7, 22];
+
+ages.forEach(age => console.log(70-age + "years to death"));
