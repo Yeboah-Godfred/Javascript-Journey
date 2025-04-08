05050a5415904631da3d5bf66a2b4157bb63021a
Yeboah-Godfred <ygodfred3@gmail.com>
2025-04-08 11:46:10 +0000

    Counter component done

--- CorpComment/script.js
+++ CorpComment/script.js
@@ -1,7 +1,20 @@
 const textareaEl = document.querySelector('.form__textarea');
 
+const counterEl = document.querySelector('.counter');
+
 const inputHandler = () => {
-   
+   //max characters
+   const maxChars = 150;
+
+   //characters entered
+   const charsEntered = textareaEl.value.length;
+
+   //characters left
+   const charsLeft = maxChars - charsEntered;
+
+   //output results
+
+   counterEl.textContent = charsLeft;
 };
 
 textareaEl.addEventListener('input', inputHandler);
